5a1b1e19cf8a9be3be115f20e7700810afb98f20
Christian Grail <christian.grail@sap.com>
2017-11-08 16:01:23 +0100

    Add empty playlists info

--- kidify/PlayListsViewController.swift
+++ kidify/PlayListsViewController.swift
@@ -31,7 +31,23 @@ class PlayListsViewController: UITableViewController {
     
     //MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        var numOfSections: Int = 0
+        if playlists.count > 0
+        {
+            tableView.separatorStyle = .singleLine
+            numOfSections            = 1
+            tableView.backgroundView = nil
+        }
+        else
+        {
+            let noDataLabel: UILabel     = UILabel(frame: CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: tableView.bounds.size.height))
+            noDataLabel.text          = "Please add some playlists"
+            noDataLabel.textColor     = UIColor.black
+            noDataLabel.textAlignment = .center
+            tableView.backgroundView  = noDataLabel
+            tableView.separatorStyle  = .none
+        }
+        return numOfSections
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
